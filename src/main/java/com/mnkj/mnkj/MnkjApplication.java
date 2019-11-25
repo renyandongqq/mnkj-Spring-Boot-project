6f4ab44e4c50b02c52c22f0afe67b5a8853e0032
renyandong <test@renyangdong.com>
2019-11-25 19:31:08 +0800

    first commit

---
+++ src/main/java/com/mnkj/mnkj/MnkjApplication.java
@@ -0,0 +1,14 @@
+package com.mnkj.mnkj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MnkjApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MnkjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mnkj/mnkj/MnkjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mnkj.mnkj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MnkjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
