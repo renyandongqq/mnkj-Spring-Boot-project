a5775eb9e83145e9f9fe372239d70b90463c7cde
renyandong <test@renyangdong.com>
2019-11-25 20:37:04 +0800

    add index and bootstrap

--- src/main/java/com/mnkj/mnkj/controller/IndexController.java
+++ src/main/java/com/mnkj/mnkj/controller/IndexController.java
@@ -6,11 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class LoginController {
+public class IndexController {
 
-    @GetMapping("/login")
-    public String login(@RequestParam(name = "name") String name, Model model){
-        model.addAttribute("name",name);
-        return "login";
+    @GetMapping("/")
+    public String login(){
+        return "index";
     }
 }
